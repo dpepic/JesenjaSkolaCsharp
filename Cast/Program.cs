57cfa2ecee64e1f3cf81c73e13bf90df5e2a1cf8
Dragan Pepic <d.pepic@hotmail.com>
2019-10-15 20:50:26 +0200

    Add project files.

---
+++ Cast/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nekiBajt;
+            nekiBajt = 6;
+
+            int ceoBroj; 
+            ceoBroj = nekiBajt; //implicitan casting
+            //implicitan cast je moguc jedino
+            //u slucaju kada ne postoji sansa
+            //da se izgubi ili osteti podatak
+            //Znaci, po pravilu, kada castamo
+            //manji tip podatka u veci
+
+
+            int ceoBroj2 = 6;
+            byte nekiBajt2;
+
+            nekiBajt2 = (byte)ceoBroj2;//ekspicitan casting
+            //Ako zelimo da prebacimo veci tip
+            //podatka u manji cast moramo da 
+            //naznacimo tako sto stavimo zeljeni
+            //tip podatka u zagrade
+
+
+            Console.WriteLine($"nekiBajt je: {nekiBajt2} vrednost inta je {ceoBroj2}");
+
+            ceoBroj2 = 257;
+            nekiBajt2 = (byte)ceoBroj2;
+            Console.WriteLine($"nekiBajt je: {nekiBajt2} vrednost inta je {ceoBroj2}");
+            //Kao sto vidimo na ovom primeru,
+            //eksplicitno castanje odreze koliko mu treba :) 
+            //U ovom slucaju, castamo int u bajt. On uzima
+            //prvih osam bitova iz inta, ostatak u potpunosti zanemaruje
+
+
+            int cBroj = 55;
+            double brojSaOstatkom = cBroj;
+
+            double brOstatak = 3.75;
+            int cBroj2 = (int)brOstatak;
+
+            Console.WriteLine($"{brOstatak} -- {cBroj2}");
+            //Bitno je da razumemo da je eksplicitno
+            //kastovanje "nasilno" skracivanje.
+            //U ovom primeru, mi bi 3.75 zaokruzili pre
+            //na 4, no cast samo zanenmari to sto mu je viska pa
+            //3.75 postane celobrojno 3. Isto bi bilo
+            //i sa 3.99999 :) 
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Promenljive/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promenljive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //Za sada, sve sto radimo pocinje od ove viticaste zagrade ovde, ovo iznad nas nista ne intresuje
+
+            byte bajt = 4; //Bajt je od 0 do 255 
+            sbyte bajtSaZnakom = -4; //sbyte (Signed Byte) je, pak, od -128 do 127 jer koristimo polovinu
+                                     //opsega standardnog bajta da bi prikazali negativne brojeve
+
+            int ceoBroj = 100; //int -- kratko od integer -- nam je standardan i podrazumevani nacin da predstavimo ceo 
+                               //broj. Do te mere da, kada u C# bilo gde napisemo nesto poput 5, -34 ili 868, on automatski
+                               //smatra da se radi o int vrednosti
+
+            /*
+             * Console.WriteLine -- to nam je metoda koju koristimo
+             * Unutar zagrada idu ulazni parametri, tj. informacije koje joj trebaju
+             * Ovde koristimo par stvari. sizeof() ce da nam da velicinu tipa, u bajtovim. Njen
+             * Ulazni parametar je, naravno, tip. 
+             * Pored toga, svaki tip je u stanju da nam kaze koja je minimalna vrednost koju mozemo da stavimo
+             * u njega, kao i maksimalna, tako sto kazmo tip.Min ili MaxValue. Npr, long.MaxValue */
+            Console.WriteLine("Jedan int zauzima " + sizeof(int) + " bajta.");
+            Console.WriteLine("Minimalna vrednost mu je " + int.MinValue + " a maksimalna " + int.MaxValue +".");
+            Console.WriteLine(); //Posto WriteLine ispise jednu liniju pa se onda spusti u drugu, ako hocu samo
+                                 //da napravim razmak izmedju dve linije teksta, pozovem ga bez parametara,
+                                 //kao sto sam spominjao, zagrade ipak moraju da su tu, bukvalista :), i on napravi
+                                 //bas to, samo prazan red
+
+            uint ceoBrojBezZnaka = 56; //Za razliku od bajta, kod svih ostalih tipova moramo da naznacimo
+                                       //kada nam negativne vrednosti NE trebaju (uint -- unsigned integer)
+
+            /*
+             * Primetili ste, verovatno, da je pri ispisu dosta nezgodno kombinovati promenljive sa tekstkom,
+             * non sto moramo "tekst " + promenljiva + " jos teksta.". Postoji laksi nacin.
+             * Ako string zapocnemo sa $" onda mozemo promenljiva da stavimo unutar {} u njemu i sve lepo radi :)
+             * Evo kako izgleda u praksi: */
+            Console.WriteLine($"Jedan uint zauzima {sizeof(uint)} bajta."); //Primetite koliko je ovo lakse
+                                                                            //od proslih WriteLine poziva :)
+            Console.WriteLine($"Minimalna vrednost mu je {uint.MinValue} a maksimalna {uint.MaxValue}.");
+            Console.WriteLine();
+
+            long velikiCeoBroj = 5;
+            Console.WriteLine($"Jedan long zauzima {sizeof(long)} bajta."); 
+            Console.WriteLine($"Minimalna vrednost mu je {long.MinValue} a maksimalna {long.MaxValue}.");
+            Console.WriteLine();
+            ulong velikiCeoBrojBezZnaka = 33;
+            Console.WriteLine($"Jedan ulong zauzima {sizeof(ulong)} bajta.");
+            Console.WriteLine($"Minimalna vrednost mu je {ulong.MinValue} a maksimalna {ulong.MaxValue}.");
+            Console.WriteLine();
+
+            //Brojevi sa ostatkom -- primetite da oni nemaju varijante koje su samo pozitivne
+            float ostatak = (float)5.5; //Relativno neprecizan broj sa ostatkom, float se retko kada koristi
+                                        //za c#, cim vidi nesto sa ostatkom, smatra da je to double, koji se
+                                        //koristi vecinu vremena
+            Console.WriteLine($"Jedan float zauzima {sizeof(float)} bajta.");
+            Console.WriteLine($"Minimalna vrednost mu je {float.MinValue} a maksimalna {float.MaxValue}.");
+            Console.WriteLine(); //primetite kod ispisa da se koristi naucna notacija, posto su brojevi veliki
+                                 //3.402823E+38 mu dodje 3.402823 * 10 na 38 :)
+
+            double ostatakPrecizan = 5.8; //Double koristimo za brojeve sa ostatkom uobicajeno
+            Console.WriteLine($"Jedan double zauzima {sizeof(double)} bajta.");
+            Console.WriteLine($"Minimalna vrednost mu je {double.MinValue} a maksimalna {double.MaxValue}.");
+            Console.WriteLine();
+
+            decimal ostatakUltraPrecizan = (decimal)3.5; //izrazito precizan broj sa ostatkom,
+                                                         //vecinom ga koristimo za finansijske podatke
+            Console.WriteLine($"Jedan decimal zauzima {sizeof(decimal)} bajta.");
+            Console.WriteLine($"Minimalna vrednost mu je {decimal.MinValue} a maksimalna {decimal.MaxValue}.");
+            Console.WriteLine();
+
+            char karakter = 'a'; //Tip promenljive koji pamti jedan i samo jedan karakter, pricacemo o ovome jos :) 
+            string nizKaraktera = "Ja pamtim gomilu slova :) ";
+
+            bool logika = false; //Boolean moze imati samo dve vrednost, true ili false.
+                                 //Spada u domen logicke matematike, cesto cemo da ga koristimo
+
+            Console.ReadKey(); //ReadKey nam inace sluzi da poslusamo sta ce korisnik da pritisne, no u ovom slucaju
+                               //ga koristimo da bi zadrzali aplikaciju da se ne zatvori pre no sto vidimo sta je
+                               //to ispisala :) 
+        } //Dovde je nase :) 
+    }
+}
