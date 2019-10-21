1f14e47f10e19caa863f40dcee130172d262abf3
Dragan Pepic <d.pepic@hotmail.com>
2019-10-21 22:12:40 +0200

    Promene :)

---
+++ TelefonskiImenik/TelefonskiImenik/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonskiImenik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                (string ImeIPrezime, string Telefon)[] imenik = new (string ImeIPrezime, string Telefon)[5];
+
+
+                Console.ReadKey();
+
+
+                Console.Write("Meni\n" +
+                                  "==============\n" +
+                                  "1 -- Unos\n" +
+                                  "2 -- Ispis\n" + 
+                                  "3 -- Izlaz\n" +
+                                  "==============\n" +
+                                  "Izaberite: ") ;
+                char unos = Console.ReadKey().KeyChar;
+
+                Console.WriteLine();
+                switch(unos)
+                {
+                    case '1':
+                        Console.WriteLine("Izabra liste unos!");
+                        break;
+                    case '2':
+                        Console.WriteLine("Izabrali ste ispis!");
+                        break;
+                    case '3':
+                        Console.WriteLine("Dovidjenja :)");
+                        Console.ReadKey();
+                        return;
+                    default:
+                        Console.WriteLine("Greska u unosu!");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
