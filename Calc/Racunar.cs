37133db513e1d5e6e7e5f17fcf2bd590ede9336b
Dragan Pepic <d.pepic@hotmail.com>
2019-12-03 17:04:10 +0100

    Kalkulator

---
+++ Calc/Racunar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+	class Racunar : INotifyPropertyChanged
+	{
+		private decimal x;
+		public decimal X 
+		{
+			get => x;
+			set
+			{
+				x = value;
+				PropertyChanged?.Invoke(this,
+					new PropertyChangedEventArgs("X"));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void uvecajBroj(int br)
+		{
+			X = X * 10 + br;
+		}
+	}
+}
